f99539ed139c12548bc9444bed26d22795401b06
coapango-ciss <20233tn218@utez.edu.mx>
2024-09-12 09:51:10 -0600

    [ADD] Se creo la estructura de la clase PET para la creación de su tabla en MYSQL

--- src/main/java/utez/edu/mx/myApi/pet/Pet.java
+++ src/main/java/utez/edu/mx/myApi/pet/Pet.java
@@ -1,5 +1,82 @@
 package utez.edu.mx.myApi.pet;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Pet")
 public class Pet {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "height", nullable = false)
+    private int height;
+
+    @Column(name = "weight", nullable = false)
+    private int weight;
+
+    public Pet() {
+    }
+
+    public Pet(long id, String nickname, int height, int weight) {
+        this.id = id;
+        this.nickname = nickname;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Pet(String nickname, int height, int weight) {
+        this.nickname = nickname;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
+
 }
+
